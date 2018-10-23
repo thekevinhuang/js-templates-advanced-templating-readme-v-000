c9d0665e117372473e3eddf963259dc9bd2fab7e
Kevin Huang <yellow.khaos@gmail.com>
2018-10-23 02:03:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,5 +2,6 @@
 function loadIssues() {
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML)
   var result = template(issues)
-  document.getElementByTagName("main")[0].innerHTML+=result
+  document.getElementsByTagName("main")[0].innerHTML+=result
 }
+
